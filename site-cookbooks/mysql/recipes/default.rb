0949459dd9575e2cc83072da49464e3b200e89f6
kazuki.fujikawa <k.fujikawa1014@gmail.com>
2014-11-10 02:13:47 +0900

    mysql setup update

--- site-cookbooks/mysql/recipes/default.rb
+++ site-cookbooks/mysql/recipes/default.rb
@@ -7,6 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
+my_cnf_path  = (node['mysql']['version'] == '5.6')? '/usr/my.cnf' : '/etc/my.cnf'
+
 bash 'remove_installed_mysql' do
   only_if 'yum list installed | grep mysql*'
   user 'root'
@@ -33,5 +35,60 @@ node['mysql']['rpm'].each do |rpm|
     action :install
     provider Chef::Provider::Package::Rpm
     source "/tmp/#{rpm['rpm_file']}"
+  end
+end
+
+template "#{my_cnf_path}" do
+  user 'root'
+  group 'root'
+  mode '0644'
+  source 'my.cnf.erb'
+
+  variables ({
+    :server_charset                  => node['mysql']['server_charset'],
+    :max_connections                 => node['mysql']['max_connections'],
+    :query_cache_size                => node['mysql']['query_cache_size'],
+    :table_cache_size                => node['mysql']['table_cache_size'],
+    :thread_cache_size               => node['mysql']['thread_cache_size'],
+    :join_buffer_size                => node['mysql']['join_buffer_size'],
+    :sort_buffer_size                => node['mysql']['sort_buffer_size'],
+    :read_rnd_buffer_size            => node['mysql']['read_rnd_buffer_size'],
+    :innodb_file_per_table           => node['mysql']['innodb_file_per_table'],
+    :innodb_data_file_path           => node['mysql']['innodb_data_file_path'],
+    :innodb_autoextend_increment     => node['mysql']['innodb_autoextend_increment'],
+    :innodb_buffer_pool_size         => node['mysql']['innodb_buffer_pool_size'],
+    :innodb_additional_mem_pool_size => node['mysql']['innodb_additional_mem_pool_size'],
+    :innodb_write_io_threads         => node['mysql']['innodb_write_io_threads'],
+    :innodb_read_io_threads          => node['mysql']['innodb_read_io_threads'],
+    :innodb_log_buffer_size          => node['mysql']['innodb_log_buffer_size'],
+    :innodb_log_file_size            => node['mysql']['innodb_log_file_size'],
+    :innodb_flush_log_at_trx_commit  => node['mysql']['innodb_flush_log_at_trx_commit']
+  })
+end
+
+service 'mysql' do
+  action [:start, :enable]
+end
+
+# version 5.6 over
+if node["mysql"]["version"].to_f >= 5.6
+  package 'expect' do
+    only_if 'ls /root/.mysql_secret'
+    :install
+  end
+
+  cookbook_file '/tmp/password_set' do
+    only_if 'ls /root/.mysql_secret'
+    source "password_set"
+  end
+
+  execute 'password_set' do
+    only_if 'ls /root/.mysql_secret'
+    user 'root'
+    command 'chmod +x /tmp/password_set && /tmp/password_set && rm -f /tmp/password_set'
+  end
+
+  package 'expect' do
+    :remove
   end
 end
---
+++ site-cookbooks/vagrant/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: vagrant
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+link "/home/vagrant/share" do
+    to "/vagrant"
+end
