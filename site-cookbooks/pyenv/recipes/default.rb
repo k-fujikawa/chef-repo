2bb1a0774e1c7c9959382fefa8349d2d9d1f6114
kazuki.fujikawa <kazuki.fujikawa@dena.jp>
2015-09-16 23:36:23 +0900

    pyenv updated

--- site-cookbooks/pyenv/recipes/default.rb
+++ site-cookbooks/pyenv/recipes/default.rb
@@ -12,6 +12,11 @@ local_user   = node['pyenv']['local_user']
 local_group  = node['pyenv']['local_group']
 profile_path = local_user == 'root' ? '/etc/profile' : '~/.bashrc'
 
+directory software_dir do
+  owner local_user
+  group local_group
+end
+
 git node['pyenv']['software_dir'] do
   user node['pyenv']['local_user']
   group node['pyenv']['local_group']
@@ -32,9 +37,6 @@ bash "pyenv" do
     eval "$(pyenv init -)"
     CONFIGURE_OPTS="--enable-shared --enable-unicode=ucs4" pyenv install #{node['pyenv']['version']}
     pyenv global #{node['pyenv']['version']}
-    echo 'export PYENV_ROOT="#{software_dir}"' >> #{profile_path}
-    echo 'export PATH="#{software_dir}/bin:$PATH"' >> #{profile_path}
-    echo 'eval "$(pyenv init -)"' >> #{profile_path}
   EOC
 
   not_if { File.exists?("#{software_dir}/shims/python") }
